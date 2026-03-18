894d4b60a236f191f5ef2754cc64ae6d6eb55e32
wendycrisponce-svg <wendycrisponce@gmail.com>
2026-03-18 15:39:19 -0300

    inicio
    inicio- prueba para trabajr en equipo
    
    usar este codigo y ampliar.Subirlo como una rama

---
+++ inicio/hola.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    printf("hola, soy wendy");
+    printf("doy inicio a la prueba de usar repositorios en git");
+   //la idea es que se pueda lograr el uso de repositorios en git y ademas, 
+   //que se logre el uso de ramas para poder trabajar en equipo 
+   //y no tener conflictos a la hora de subir los cambios al repositorio principal
+   //o usar las ramas
+
+
+
+    return (0);
+
+}
